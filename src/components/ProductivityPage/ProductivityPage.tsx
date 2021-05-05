e56e8405e24f394a5ec9d4a6de0b95abb3196c85
daisuke-awaji <daisuke.awaji@i.softbank.jp>
2021-05-05 10:46:51 +0900

    save repo name into query

--- src/components/ProductivityPage/ProductivityPage.tsx
+++ src/components/ProductivityPage/ProductivityPage.tsx
@@ -11,22 +11,30 @@ import { useRepositoryStatusApi } from '../../api/useRepositoryStatus';
 import { Loading } from '../Atoms/Loading';
 import { DateRange } from 'materialui-daterange-picker';
 import { sub } from 'date-fns';
+const { useQueryParams } = require('react-router-query-hooks');
 
 const ProductivityPage = () => {
+  const [query, { replaceQuery }] = useQueryParams();
+  console.log(query);
+
   const { repositories, isLoading } = useRepositoryStatusApi();
   const repoNames = repositories.map((repo) => repo.nameWithOwner);
   const [selectedRepository, setSelectedRepository] = useState('');
-  const [dateRange, setDateRange] = React.useState<DateRange>({
+
+  const initialDateRange = {
     startDate: sub(new Date(), { months: 1 }),
     endDate: new Date(),
-  });
+  };
+  const [dateRange, setDateRange] = React.useState<DateRange>(initialDateRange);
 
   const handleChange = (event: any) => {
     setSelectedRepository(event.target.value);
+    replaceQuery({ repo: event.target.value });
   };
 
   useEffect(() => {
-    setSelectedRepository(repoNames[0]);
+    const repo = query.repo || repoNames[0];
+    setSelectedRepository(repo);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [repositories]);
 
@@ -57,7 +65,10 @@ const ProductivityPage = () => {
       </Grid>
 
       <Grid item xs={12} sm={12} md={12} lg={12}>
-        <ProductionLeadTimeCard repository={selectedRepository} dateRange={dateRange} />
+        <ProductionLeadTimeCard
+          repository={selectedRepository}
+          dateRange={dateRange}
+        />
       </Grid>
 
       <Grid item xs={12} sm={12} md={12} lg={6}>
