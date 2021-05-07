86bafe0c8cfb97cee80bfed69fcedf3b86ec6e26
awaji-daisuke <awaji_daisuke@intec.co.jp>
2021-05-07 13:14:53 +0900

    request timeout 29000

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,9 +1,9 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const createClient = (token: string) => {
   const gitscopeApiClient = axios.create({
     baseURL: process.env.REACT_APP_BACKEND_API_ENDPOINT,
-    timeout: 10000,
+    timeout: 29000,
     headers: { Authorization: `Bearer ${token}` },
   });
 
